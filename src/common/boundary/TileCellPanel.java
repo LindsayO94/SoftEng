835648015f50757a62ed42a6413f6455f812b61d
beiju <beiju@me.com>
2015-05-03 15:02:33 -0400

    Deleted extra files and fixed simple changes

--- src/common/boundary/TileCellPanel.java
+++ src/common/boundary/TileCellPanel.java
@@ -32,9 +32,9 @@ public class TileCellPanel extends CellPanel {
 	
 	public void refresh() {
 		if (cell.isSelected()) {
-			setBorder(new LineBorder(new Color(0, 255, 255), 1));
+			setBorder(new LineBorder(new Color(0, 255, 255), 2));
 		} else if (cell.isMarked()) {
-			setBorder(new LineBorder(new Color(255, 255, 255), 1));
+			setBorder(new LineBorder(new Color(255, 255, 255), 2));
 		} else {
 			setBorder(new LineBorder(new Color(0, 0, 0), 1));
 		}
