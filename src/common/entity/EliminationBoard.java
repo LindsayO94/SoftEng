051bbebd1791212102918b1ec3354fa4a780d110
Artian Kica <artiankica@hotmail.com>
2015-05-03 12:16:52 -0400

    updated some boards, their win conditions, and level attributes to reflect this

--- src/common/entity/EliminationBoard.java
+++ src/common/entity/EliminationBoard.java
@@ -4,16 +4,21 @@ package common.entity;
 public class EliminationBoard extends Board {
 
 	int movesLeft;
+	int cellsLeftToMark;
 	
 	public EliminationBoard(Level level) {
 		super(level);
 		
 		this.movesLeft = level.maxMoves;
+		this.cellsLeftToMark = level.getNumActiveCells();
 	}
 
 	@Override
 	public boolean isWon() {
 		// mark all cells
+		if (cellsLeftToMark == 0){
+			return true;
+		}
 		return false;
 	}
 
