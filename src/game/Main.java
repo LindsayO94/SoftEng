a5b4b74aee78ec291f23fc738fc5d6d2d7b97a55
beiju <beiju@me.com>
2015-05-05 17:35:16 -0400

    Mark in elimination mode

--- src/game/Main.java
+++ src/game/Main.java
@@ -8,24 +8,10 @@ import game.entities.*;
 import game.boundary.*;
 
 public class Main {
-	public static ArrayList<Level> addLevels(ArrayList<Level> levels){
-		for (String type : Level.allowedTypes) {
-			for (int score = 100; score <= 500; score += 100) {
-				Level l = new Level(type, 0);
-				l.setHighScore(score);
-				levels.add(l);
-			}
-		}
-		return levels;
-	}
-	
 	public static void main(String args[]) {
-		ArrayList<Level> levels = new ArrayList<Level>();
-		levels = addLevels(levels);
-		Game game = new Game(levels);
-		//System.out.println(game.iter.next().getHighScore());
+		Game game = new Game();
 		GameFrame f = new GameFrame(game);
-		f.setVisible(true);		
+		f.setVisible(true);
 	}
 	
 	
