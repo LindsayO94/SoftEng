188b2b505375fc47981cd00acef20b073c075d03
Lindsay O'Donnell <lindsayrose03@yahoo.com>
2015-05-06 03:38:38 -0400

    Here have some documentation

---
+++ src/common/boundary/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides the classes for the boundary objects shared between the Game and LevelBuilder.
+ * These are the Board, various types of Cells, Tiles, and the LevelSelectPanel.
+ * 
+ */
+package common.boundary;
---
+++ src/common/entity/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides the classes for the entity objects shared between the Game and LevelBuilder.
+ * These are the four types of Boards, various types of Cells, Tiles, and the Level.
+ * 
+ */
+package common.entity;
